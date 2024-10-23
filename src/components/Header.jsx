dffbc155abaf35095b9116f5b1003e03bc9f78c0
aieest <aie.estidola@gmail.com>
2024-10-23 18:46:31 +1300

    added header placeholders

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='logo'>Logo</div>
+
+      <span className="material-icons-outlined">
+        search
+      </span>
+    </header>
   )
 }
 

c5f282f131fa8c0c4061cb4fad8cfadbb8676e4e
aieest <aie.estidola@gmail.com>
2024-10-25 17:49:00 +1300

    Added navbar placeholder icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,23 @@ import React from "react";
 
 function Navbar() {
     return(
-        <div>Navbar</div>
+        <nav>
+            <span className="nav-home-link material-icons-outlined">
+                home
+            </span>
+            <span className="nav-notif-link material-icons-outlined">
+                notifications
+            </span>
+            <span className="nav-create-link material-icons-outlined">
+                add
+            </span>
+            <span className="nav-messages-link material-icons-outlined">
+                sms
+            </span>
+            <span className="nav-person-link material-icons-outlined">
+                person
+            </span>
+        </nav>
     );
 };
 

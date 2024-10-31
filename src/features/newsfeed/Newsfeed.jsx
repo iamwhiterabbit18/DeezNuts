2cc2a175b06eac9397bbc23d3f67f3e73a2ee099
aieest <aie.estidola@gmail.com>
2024-10-31 17:38:01 +1300

    Updated HTML and imported styles

--- src/features/newsfeed/Newsfeed.jsx
+++ src/features/newsfeed/Newsfeed.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Header from '@components/Header'
 import Navbar from '@components/Navbar'
+import styles from './newsfeed.module.scss'
+
 function Newsfeed() {
   return (
-    <>
-    <Header />
-    <main>
-      <div>Newsfeed</div>
-      <Navbar />
-    </main>
-    </>
+
+    <section className={styles.nfContainer}>
+        <Header />
+      <main className={styles.nfMainContainer}>
+        <div>Will add card here</div>
+        <Navbar />
+      </main>
+    </section>
+
   )
 }
 

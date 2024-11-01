e2afadbae60ee069d76997be8809e0f9a6bbb131
aieest <aie.estidola@gmail.com>
2024-11-02 10:11:23 +1300

    Created PostCard.jsx

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./components.module.scss"
+
+function PostCard() {
+    return(
+        <div>
+            
+        </div>
+    );
+};
+
+export default PostCard;
